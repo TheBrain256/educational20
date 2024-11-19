49ab97d02c691332bea0a5bed6746b6945ead4df
TheBrain256 <Shimoro2576@gmail.com>
2024-11-19 16:53:41 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fdivide
+from true_math import divide as tdivide
+
+result1 = fdivide(55,5)
+result2 = fdivide(15,0)
+result3 = tdivide(36, 4)
+result4 = tdivide(98, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
